ecd91ea27edc03936d64538238b8c88bb5ee7bc1
berial <715434384@qq.com>
2016-01-25 20:10:25 +0800

    update

--- src/android/SocialSharing.java
+++ src/android/SocialSharing.java
@@ -24,6 +24,8 @@ import cn.sharesdk.wechat.moments.WechatMoments;
  */
 public class SocialSharing extends CordovaPlugin {
 
+    private CallbackContext _callback;
+
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -32,21 +34,22 @@ public class SocialSharing extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        _callback = callbackContext;
         if ("shareWeibo".equals(action)) {
-            _share(SinaWeibo.NAME, args.getString(0), callbackContext);
+            _share(SinaWeibo.NAME, args.getString(0));
         } else if ("shareQQ".equals(action)) {
-            _share(QQ.NAME, args.getString(0), callbackContext);
+            _share(QQ.NAME, args.getString(0));
         } else if ("shareWeixin".equals(action)) {
-            _share(Wechat.NAME, args.getString(0), callbackContext);
+            _share(Wechat.NAME, args.getString(0));
         } else if ("shareWeixinMoment".equals(action)) {
-            _share(WechatMoments.NAME, args.getString(0), callbackContext);
+            _share(WechatMoments.NAME, args.getString(0));
         } else if ("shareSMS".equals(action)) {
-            _share(ShortMessage.NAME, args.getString(0), callbackContext);
+            _share(ShortMessage.NAME, args.getString(0));
         }
         return false;
     }
 
-    private void _share(String platName, String message, final CallbackContext callbackContext) {
+    private void _share(String platName, String message) {
         OnekeyShare oks = new OnekeyShare();
         oks.disableSSOWhenAuthorize();
         oks.setPlatform(platName);
@@ -60,17 +63,17 @@ public class SocialSharing extends CordovaPlugin {
 
             @Override
             public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
-                callbackContext.success(88);
+                _callback.success(i);
             }
 
             @Override
             public void onError(Platform platform, int i, Throwable throwable) {
-                callbackContext.success(1);
+                _callback.error(i);
             }
 
             @Override
             public void onCancel(Platform platform, int i) {
-                callbackContext.success(0);
+                _callback.error(i);
             }
         });
         oks.show(cordova.getActivity());
